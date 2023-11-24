37735905486c365f832298758f91b15b6d294207
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2023-11-24 20:25:23 +0000

    Removed hardcoded menu item and added js functions that loop through the data and render each item dynamically

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 import { menuArray } from "./data.js";
 
-console.log(menuArray);
+const getMenuHTML = () => {
+    return menuArray.map(item => {
+        const {name, ingredients, id, price, emoji} = item;
+        
+        return `                
+            <div class="item-container">
+                <p class="item-emoji">${emoji}</p>
+                <div class="item-text-container">
+                    <p class="item-name">${name}</p>
+                    <p class="item-ingredients">${ingredients.join(",")}</p>
+                    <p class="item-price">$${price}</p>
+                </div>
+                <button class="add-button">+</button>
+            </div>
+        `;
+    }).join("");
+};
+
+const renderMenu = () => document.getElementById("menu").innerHTML = getMenuHTML();
+
+renderMenu();
